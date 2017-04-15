50cb4bb5d970a238c43d34f2272de492b25e495e
treziac <aymeric.bouvet@outlook.com>
2017-04-15 13:25:25 +0200

    Add Console project

---
+++ ConfluentInConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfluentInConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("librdakfka " + Confluent.Kafka.Library.VersionString + " correctly loaded");
+        }
+    }
+}
